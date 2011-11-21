d3207d2a5904b96d05963fff9c88185f87217562
AHKjeldsen <andukjeldsen@hotmail.com>
2011-11-21 17:13:41 +0100

    Setup projects. Added TODO list.

---
+++ source/PirateSpades/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PirateSpades {
+    class Program {
+        static void Main(string[] args) {
+            // Let's rock an roll!
+        }
+    }
+}
