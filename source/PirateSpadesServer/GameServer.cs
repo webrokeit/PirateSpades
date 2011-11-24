af09501af02b0953225ffaa836a6c098dffe8c66
Andreas Kjeldsen <andukjeldsen@hotmail.com>
2011-11-24 09:59:27 +0100

    Started working on the server. Bon and c# code partially written.

--- source/PirateSpadesServer/GameServer.cs
+++ source/PirateSpadesServer/GameServer.cs
@@ -12,14 +12,14 @@ namespace PirateSpadesServer {
         private TcpListener Listener { get; set; }
         private int Port { get; set; }
 
-        private List<Socket> Players { get; set; }
+        private HashSet<Socket> Players { get; set; }
 
         private IAsyncResult asyncAcceptResult;
 
         public GameServer(int port) {
             this.Port = port;
 
-            this.Players = new List<Socket>();
+            this.Players = new HashSet<Socket>();
             this.Listener = new TcpListener(IPAddress.Parse("127.0.0.1"), this.Port);
         }
 
@@ -33,11 +33,25 @@ namespace PirateSpadesServer {
         }
 
         private void WaitForSocket() {
-            this.Listener.BeginAcceptSocket(this.SocketConnected, Listener);
+            this.Listener.BeginAcceptSocket(this.SocketConnected, null);
         }
 
         private void SocketConnected(IAsyncResult ar) {
-            // lol
+            Socket client = this.Listener.EndAcceptSocket(ar);
+            if(!Players.Contains(client)) {
+                Players.Add(client);
+                
+            }else {
+                // TODO: Notify client that they're already conncected.
+            }
+        }
+
+        private void WaitForSocketMessage(Socket client) {
+            // TODO: Make it wait
+        }
+
+        private void SocketMessageReceived(IAsyncResult ar) {
+            // TODO: receive message and do with it whatever needs done
         }
     }
 }
