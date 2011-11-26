e11bd6edebedffa603a3e447b15b9f21f0e74a65
helenakruger <helenakruger@hotmail.com>
2011-11-26 19:50:53 +0100

    The Algorithm with the files. Forgot them last time i pushed.

---
+++ source/PirateSpades/Bet/Betting.cs
@@ -0,0 +1,19 @@
+// Helena
+
+namespace PirateSpades.Bet
+{
+    public class Betting {
+
+        private int bet;
+        private bool showBet;
+    
+        public Betting() {
+            this.bet = 0;
+            this.showBet = false;
+        }
+
+        public void makeBet(Player p, int b) {
+            
+        }
+    }
+}
---
+++ source/PirateSpades/Func/Fisher-Yates-Alg.cs
@@ -0,0 +1,37 @@
+﻿// Helena 
+
+namespace PirateSpades.Func
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
+
+    /// <summary>
+    /// A shuffle algorithm, which is unbiased. Iterates through the whole collection and swaps the item at index i, with an item 
+    /// at a randomly generated index in the collection. Does this until there's only one item left.
+    /// </summary>
+    public class FisherYatesAlg
+    {
+        private Random r;
+
+        public FisherYatesAlg() {
+            this.r = new Random();
+        }
+
+        public void Algorithm<T>(IList<T> c) {
+            Contract.Requires(c.Count != 0);
+            Contract.Ensures(c.Count == Contract.OldValue(c.Count));
+
+            if (c.Count > 1) {
+                int i = c.Count - 1;
+                while (i > 1) {
+                    int s = r.Next(i);
+                    T holder = c[i];
+                    c[i] = c[s];
+                    c[s] = holder;
+                    i--;
+                }
+            }
+        }
+    }
+}
