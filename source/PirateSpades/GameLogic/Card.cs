764ab119f17ec365ce46de68d3c83d8c668e7f09
MCEskesen <copinhome@gmail.com>
2011-11-27 14:49:03 +0100

    Added Card.cs, Player.cs, Deck.cs and Table.cs.. Table.cs is not even close to being finished.. But the others however are implemented according to their formal bon specification (so changes can be made).. The classes Player, Deck, Card, Suit and CardValue are therefore implemented.. Suit and CardValue are in the Card.cs file.

---
+++ source/PirateSpades/GameLogic/Card.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace PirateSpades.GameLogic {
+    
+    public class Card {
+        private readonly Suit s;
+        private readonly CardValue v;
+
+        public Card(Suit s, CardValue v) {
+            this.s = s;
+            this.v = v;
+        }
+
+        public CardValue Value { get { return v; } }
+
+        public Suit Suit { get { return s; } }
+
+        //Returns true if the card is greater than the card given as parameter
+        public bool Greater(Card c) {
+            Contract.Requires(c != null);
+            if(c.Suit == Suit) {
+                return Value < c.Value;
+            }
+            if(Suit == Suit.Spades) {
+                return true;
+            }
+            return false;
+        }
+
+        public bool SameSuit(Card c) {
+            Contract.Requires(c != null);
+            Contract.Ensures(Suit == c.Suit ? Contract.Result<bool>() : true);
+            return Suit == c.Suit;
+        }
+
+        [ContractInvariantMethod]
+        private void ObjectInvariant() {
+            Contract.Invariant(Value > (CardValue)1 && Value < (CardValue)15);
+        }
+    }
+
+    public enum Suit {
+        Hearts, Spades, Diamonds, Clubs
+    }
+
+    public enum CardValue {
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Jack = 11,
+        Queen = 12,
+        King = 13,
+        Ace = 14
+    }
+}
