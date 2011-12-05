c41b2ab574b8207e1d6c568d429a5bcc1ecdbc23
helenakruger <helenakruger@hotmail.com>
2011-12-05 14:36:48 +0100

    Merge

--- source/PirateSpadesGame/PirateSpadesGame/PSGame.cs
+++ source/PirateSpadesGame/PirateSpadesGame/PSGame.cs
@@ -1,21 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
 
 namespace PirateSpadesGame {
     /// <summary>
     /// This is the main type for your game
     /// </summary>
     public class PsGame : Microsoft.Xna.Framework.Game {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+        private float acc = 0;
 
         public PsGame() {
             graphics = new GraphicsDeviceManager(this);
