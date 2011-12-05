47d3e62b50287a90dd25d93f0eeaf5459253a166
MCEskesen <copinhome@gmail.com>
2011-12-05 13:11:50 +0100

    Made a start screen for the game with a menu and prepared for to-be added functionalities..

--- source/PirateSpadesGame/PirateSpadesGame/Sprite.cs
+++ source/PirateSpadesGame/PirateSpadesGame/Sprite.cs
@@ -17,8 +17,15 @@ namespace PirateSpadesGame {
         public float Scale = 1.0f;
         private Player player;
 
+        public Sprite() {
+            Color = Color.White;
+        }
 
         public Player Player { get { return player; } }
+
+        public Color Color { get; set; }
+
+        public Texture2D Tex { get { return mSpriteTexture; } }
 
         public void LoadContent(ContentManager theContentManager, string theAssetName) {
             mSpriteTexture = theContentManager.Load<Texture2D>(theAssetName);
@@ -28,7 +35,7 @@ namespace PirateSpadesGame {
         public void Draw(SpriteBatch theSpriteBatch) {
             theSpriteBatch.Draw(mSpriteTexture, Position,
                 new Rectangle(0, 0, mSpriteTexture.Width, mSpriteTexture.Height),
-                Color.White, 0.0f, Vector2.Zero, Scale, SpriteEffects.None, 0);
+                Color, 0.0f, Vector2.Zero, Scale, SpriteEffects.None, 0);
         }
     }
 }
